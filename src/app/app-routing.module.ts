2e07d72ae0f048589843a9c87e055cab9284592f
alan-serrano <alan.serrano.pch@gmail.com>
2019-09-26 16:40:28 -0400

    Diseño Preliminar y creación de components

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,23 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { HomeComponent } from './components/home/home.component';
+import { LoginComponent } from './components/login/login.component';
+import { ChatComponent } from './components/chat/chat.component';
+import { RegistroComponent } from './components/registro/registro.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: HomeComponent },
+  { path: 'login', component: LoginComponent },
+  { path: 'chat', component: ChatComponent },
+  { path: 'registro', component: RegistroComponent },
+  { path: '**', redirectTo: '' },
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forRoot(routes, {useHash: true})],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
+
+
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,6 +7,10 @@ import { HeadChatComponent } from './components/head-chat/head-chat.component';
 import { ContactosComponent } from './components/contactos/contactos.component';
 import { HistorialComponent } from './components/historial/historial.component';
 import { HeaderComponent } from './components/header/header.component';
+import { LoginComponent } from './components/login/login.component';
+import { HomeComponent } from './components/home/home.component';
+import { ChatComponent } from './components/chat/chat.component';
+import { RegistroComponent } from './components/registro/registro.component';
 
 @NgModule({
   declarations: [
@@ -14,7 +18,11 @@ import { HeaderComponent } from './components/header/header.component';
     HeadChatComponent,
     ContactosComponent,
     HistorialComponent,
-    HeaderComponent
+    HeaderComponent,
+    LoginComponent,
+    HomeComponent,
+    ChatComponent,
+    RegistroComponent
   ],
   imports: [
     BrowserModule,
